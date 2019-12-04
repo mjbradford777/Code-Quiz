e02e3519fd9f7fcbbd54e3d16a68c43b2834e0da
Red Skull <bradfordmichael7@gmail.com>
2019-12-03 21:18:25 -0500

    added local storage of highscores and modal to display along with option to clear

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,9 @@ let title = document.getElementById('quiz-title');
 let instr = document.getElementById('instructions');
 let btn = document.getElementById('start-button');
 let time = document.getElementById('time');
+let highScoreTrigger = document.getElementById('high-scores');
+let modalBody = document.getElementById('modal-body');
+let clear = document.getElementById('clear');
 let h2;
 let btns;
 let newElement;
@@ -39,6 +42,28 @@ let correct;
 let questionCounter;
 let seconds;
 let retry;
+let score;
+let form;
+let initials;
+let highScores = [];
+let highScoreInitials = [];
+let storedHighScores;
+let storedInitials;
+let localInitials = [];
+let localScores = [];
+
+const init = () => {
+    storedHighScores = JSON.parse(localStorage.getItem("localScores"));
+    if (storedHighScores !== null) {
+        highScores = storedHighScores;
+    }
+    storedInitials = JSON.parse(localStorage.getItem("localInitials"));
+    if (storedInitials !== null) {
+        highScoreInitials = storedInitials;
+    }
+    console.log(highScores);
+    console.log(highScoreInitials);
+}
 
 const timer = () => {
     if (!isNaN(seconds)){
@@ -103,6 +128,7 @@ const advanceQuestion = () => {
         })
     } else if (seconds >= 0) {
         time.textContent = seconds;
+        score = seconds;
         newElement = document.createElement('h2');
         newElement.textContent = 'Good job!';
         newElement.id = 'good';
@@ -111,6 +137,21 @@ const advanceQuestion = () => {
         newElement.textContent = `Your score is ${time.innerHTML}`;
         newElement.id = 'score-display';
         content.appendChild(newElement);
+        newElement = document.createElement('form');
+        newElement.setAttribute('method', 'POST');
+        newElement.id = 'score-form';
+        content.appendChild(newElement);
+        form = document.getElementById('score-form');
+        newElement = document.createElement('label');
+        newElement.setAttribute('for', 'initials');
+        newElement.textContent = 'Your initials here:';
+        form.appendChild(newElement);
+        newElement = document.createElement('input');
+        newElement.setAttribute('type', 'text');
+        newElement.setAttribute('name', 'initials');
+        newElement.id = 'initials';
+        form.appendChild(newElement);
+        initials = document.getElementById('initials');
         newElement = document.createElement('button');
         newElement.textContent = 'Retry';
         newElement.setAttribute('class', 'btn btn-success btn-block');
@@ -118,6 +159,7 @@ const advanceQuestion = () => {
         content.appendChild(newElement);
         questionCounter = 0;
         retry = document.getElementById('retry');
+        form.onsubmit = addScore;
         retry.setAttribute("onclick","document.location.reload()") ;
     } else {
         newElement = document.createElement('h2');
@@ -167,5 +209,55 @@ const checkAnswer = (Event) => {
     }
 }
 
+const addScore = (Event) => {
+    Event.preventDefault();
+
+    initialsToStore = initials.value.trim();
+    if (initialsToStore === '') {
+        return;
+    }
+    form.style.display = 'none';
+
+    highScoreInitials.push(initialsToStore);
+    highScores.push(score);
+
+    localStorage.setItem("localInitials", JSON.stringify(highScoreInitials));
+    localStorage.setItem("localScores", JSON.stringify(highScores));
+}
+
+const writeToModal = () => {
+    if (highScoreInitials.length === 0) {
+        return;
+    } else {
+        for (let i = 0; i < highScoreInitials.length; i++) {
+            newElement = document.createElement('div');
+            newElement.setAttribute('class', 'modal-divs');
+            newElement.id = `modal-div-${i}`;
+            modalBody.appendChild(newElement);
+            newElement = document.createElement('p');
+            newElement.textContent = `${highScoreInitials[i]} ${highScores[i]}`;
+            document.getElementById(`modal-div-${i}`).appendChild(newElement);
+        }
+    }
+}
+
+const clearScores = () => {
+    if (highScoreInitials.length === 0) {
+        return;
+    } else {
+        highScoreInitials = [];
+        highScores = [];
+        localStorage.setItem("localInitials", JSON.stringify(highScoreInitials));
+        localStorage.setItem("localScores", JSON.stringify(highScores));
+        for (let i = 0; i < document.getElementsByClassName('modal-divs').length; i++) {
+            document.getElementsByClassName('modal-divs')[i].style.display = 'none';
+        }
+    }
+}
+
+init();
+
 btn.onclick = startQuiz;
 document.onclick = checkAnswer;
+highScoreTrigger.onclick = writeToModal;
+clear.onclick = clearScores;
